02844703d88002559b1e9c20b665d50a38f0063d
Ing. Fabian Steiner BSc. <fabian@stei-ner.net>
2023-11-03 13:28:38 +0100

    init

--- BFS.c
+++ BFS.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include "mpi.h"
 
-void electLeader(int rank, int size, int diameter, int* leaderUID){
-    int i;
-    int max = rank;
-    int maxRank = rank;
-    int *buf = (int *)malloc(sizeof(int)*size);
-    for(i=0; i<diameter; i++){
-        MPI_Allgather(&max, 1, MPI_INT, buf, 1, MPI_INT, MPI_COMM_WORLD);
-        int j;
-        for(j=0; j<size; j++){
-            if(buf[j] > max){
-                max = buf[j];
-                maxRank = j;
-            }
-        }
-    }
-    if(rank == maxRank){
-        printf("I am the leader\n");
-    }
-}
-
-
 
 int main( argc, argv )
 int  argc;
